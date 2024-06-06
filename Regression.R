f06c4198806c6ebeb56c2531f2bfbeaf46d22fe8
SRCalduch <126208643+SRCalduch@users.noreply.github.com>
2024-06-06 23:17:06 +0200

    Regression.R

--- Regression.R
+++ Regression.R
@@ -1,51 +1,45 @@
+# Data
 library(readr)
 DoublesATP<- read_csv("TFM/DoublesATP.csv")
 colnames(DoublesATP)[1] <- "matches"
-DoublesATP$Retirement <- as.factor(DoublesATP$Retirement)
-DoublesATP$surface <- as.factor(DoublesATP$surface)
-DoublesATP$tourney_level <- as.factor(DoublesATP$tourney_level)
 
+#Transforming data
+DoublesATP$surface <- factor(DoublesATP$surface)
+DoublesATP$tourney_level <- factor(DoublesATP$tourney_level)
+DoublesATP$round_group <- factor(DoublesATP$round_group)
+DoublesATP$Retirement <- factor(DoublesATP$Retirement)
+DoublesATP$Retirement <- as.numeric(DoublesATP$Retirement)
 
-# Reg simple Surface
-Reg_Ret_Surf<-glm(Retirement~surface, family = "binomial", data = DoublesATP)
-summary(Reg_Ret_Surf)
-statistically significant
-
-# Reg simple Tourney Level
-DoublesATP$tourney_level<-relevel(DoublesATP$tourney_level, ref = "Masters 1000")
-Reg_Ret_Tourney<-glm(Retirement~tourney_level, family = "binomial", data = DoublesATP)
-summary(Reg_Ret_Tourney)
-statistically significant
-
-# Reg simple Round
-Reg_Ret_round<-glm(Retirement~round_group, family = "binomial", data = DoublesATP)
-summary(Reg_Ret_round)
-statistically significant
-
-
-# Regression model
-DoublesATP$surface <- relevel(DoublesATP$surface, ref = "Hard")
+# Poisson Regression Model
+DoublesATP$surface <- relevel(DoublesATP$surface, ref = "Grass")
 DoublesATP$tourney_level <- relevel(DoublesATP$tourney_level, ref = "Masters 1000")
-Reg_mod <- glm(formula = Retirement ~ surface + tourney_level + round_group + winner_age_mean, family = "binomial", data = DoublesATP)
+Reg_mod <- glm(formula = Retirement ~ surface + tourney_level + round_group, 
+                  family = "poisson", 
+                  offset = log(Total_Games), 
+                  data = DoublesATP)
 summary(Reg_mod)
-step(Reg_mod)
-Reg_mod_ok <- glm(formula = Retirement ~ surface + tourney_level + round_group, family = "binomial", data = DoublesATP)
-summary(Reg_mod_ok)
-par(mfrow=c(2,2))
-plot(Reg_mod_ok)
 
-library(effects)
-plot(allEffects(Reg_mod_ok))
+# Results table
+coefficients <- summary(Reg_mod)$coefficients
+estimates <- coefficients[, "Estimate"]
+std_errors <- coefficients[, "Std. Error"]
+irr <- exp(estimates)
+lower_ci <- exp(estimates - 1.96 * std_errors)
+upper_ci <- exp(estimates + 1.96 * std_errors)
 
-#Normalidad
-residues<-rstandard(Reg_mod_ok)
-par(mfrow=c(1,3))
-hist(residues)
-boxplot(residues)
-qqnorm(residues)
-qqline(residues)
+results <- data.frame(
+  Variables = rownames(coefficients),
+  Estimate = estimates,
+  SE = std_errors,
+  IRR = irr,
+  Lower_CI95 = lower_ci,
+  Upper_CI95 = upper_ci,
+  p_value = coefficients[, "Pr(>|z|)"]
+)
 
-#ANOVA
-anova_reg_mod <- anova(Reg_mod_ok,test="Chi")
-anova_reg_mod
+print(results)
 
+# Dispersion
+library(AER)
+dispersiontest(Reg_mod, trafo=1)
+dispersiontest(Reg_mod, trafo=2)
